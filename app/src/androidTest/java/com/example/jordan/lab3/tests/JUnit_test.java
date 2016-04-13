a939236665b370098972e217290f15bb13fd9ced
jbbauer <jbbauer@ucsd.edu>
2016-04-13 09:51:32 -0700

    first commit

---
+++ app/src/androidTest/java/com/example/jordan/lab3/tests/JUnit_test.java
@@ -0,0 +1,29 @@
+package com.example.jordan.lab3.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.jordan.lab3.MainActivity;
+import com.example.jordan.lab3.R;
+
+/**
+ * Created by Jordan on 4/13/16.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    MainActivity mainActivity;
+    public JUnit_test(){
+
+        super(MainActivity.class);
+    }
+
+    public void test_first()
+    {
+        mainActivity = getActivity();
+
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Bye World", tester);
+    }
+}
